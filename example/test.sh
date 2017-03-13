00b1a3f4bc052a719593c29075d1e94160a9dfd1
Jamie Hewland <jhewland@gmail.com>
2017-03-13 10:45:33 +0200

    Run tests on single container setup

--- example/test.sh
+++ example/test.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Set a trap on errors to make it clear when tests have failed
-trap '{ set +x; echo; echo FAILED; echo; } >&2' ERR
-
-# Set a trap to bring everything down when we exit
-trap "{ set +x; docker-compose down; }" EXIT
-
 # macOS-compatible timeout function: http://stackoverflow.com/a/35512328
 function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
@@ -16,22 +10,34 @@ function wait_for_log_line() {
   timeout "${LOG_TIMEOUT:-10}" grep -m 1 -E "$log_pattern" <(docker-compose logs -f "$service" 2>&1)
 }
 
+COMPOSE_FILE="${1:-docker-compose.yml}"
+
+function compose_cmd() {
+  docker-compose -f "$COMPOSE_FILE" "$@"
+}
+
+# Set a trap on errors to make it clear when tests have failed
+trap '{ set +x; echo; echo FAILED; echo; } >&2' ERR
+
+# Set a trap to bring everything down when we exit
+trap "{ set +x; compose_cmd down; }" EXIT
+
 set -x
 
 # Bring up the DB and AMQP first
-docker-compose up -d db amqp
+compose_cmd up -d db amqp
 wait_for_log_line db 'database system is ready to accept connections'
 wait_for_log_line amqp 'Server startup complete'
 
 # Bring everything else up
-docker-compose up -d
+compose_cmd up -d
 
 # Django tests
 # ############
 wait_for_log_line web 'Booting worker'
-docker-compose ps web | grep 'Up'
+compose_cmd ps web | grep 'Up'
 
-WEB_PORT="$(docker-compose port web 8000 | cut -d':' -f2)"
+WEB_PORT="$(compose_cmd port web 8000 | cut -d':' -f2)"
 
 # Simple check to see if the site is up
 curl -fsL http://localhost:$WEB_PORT/admin | fgrep '<title>Log in | Django site admin</title>'
@@ -46,7 +52,7 @@ curl -fsI http://localhost:$WEB_PORT/static/admin/img/search.7cf54ff789c6.svg | 
 curl -fsI http://localhost:$WEB_PORT/static/admin/img/search.svg | fgrep -v 'Cache-Control'
 
 # Check tables were created in the database
-[[ $(docker-compose exec --user postgres db \
+[[ $(compose_cmd exec --user postgres db \
   psql -q --dbname mysite -c "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='public';" \
     | grep -E '^\s*[[:digit:]]+' | tr -d ' ') > 0 ]]
 
@@ -55,14 +61,14 @@ curl -fsI http://localhost:$WEB_PORT/static/admin/img/search.svg | fgrep -v 'Cac
 # ############
 # Check the logs to see if the Celery worker started up successfully
 wait_for_log_line worker 'celery@\w+ ready'
-docker-compose ps worker | grep 'Up'
+compose_cmd ps worker | grep 'Up'
 
 # Check the logs to see if Celery beat started up successfully
 wait_for_log_line beat 'beat: Starting\.\.\.'
-docker-compose ps beat | grep 'Up'
+compose_cmd ps beat | grep 'Up'
 
 # Check a queue was created in RabbitMQ
-docker-compose exec amqp rabbitmqctl list_queues -p /mysite | grep 'celery'
+compose_cmd exec amqp rabbitmqctl list_queues -p /mysite | grep 'celery'
 
 set +x
 echo
