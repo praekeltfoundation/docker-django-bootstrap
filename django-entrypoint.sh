2ef586f8a72d0e08acf88513d121e13cb0cb620d
Jamie Hewland <jhewland@gmail.com>
2017-05-12 11:51:05 +0200

    Run Gunicorn/Celery as the 'django' user
    * No separate 'gunicorn' and 'celery' users

--- django-entrypoint.sh
+++ django-entrypoint.sh
@@ -17,20 +17,20 @@ if [ "$1" = 'gunicorn' ]; then
   # Do an extra chown of the /app directory at runtime in addition to the one in
   # the build process in case any directories are mounted as root-owned volumes
   # at runtime.
-  chown -R gunicorn:gunicorn /app
+  chown -R django:django /app
 
-  # Run the migration as the gunicorn user so that if it creates a local DB
+  # Run the migration as the django user so that if it creates a local DB
   # (e.g. when using sqlite in development), that DB is still writable.
   # Ultimately, the user shouldn't really be using a local DB and it's difficult
   # to offer support for all the cases in which a local DB might be created --
   # but here we do the minimum.
-  su-exec gunicorn django-admin migrate --noinput
+  su-exec django django-admin migrate --noinput
 
   if [ -n "$SUPERUSER_PASSWORD" ]; then
     echo "from django.contrib.auth.models import User
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', '$SUPERUSER_PASSWORD')
-" | su-exec gunicorn django-admin shell
+" | su-exec django django-admin shell
     echo "Created superuser with username 'admin' and password '$SUPERUSER_PASSWORD'"
   fi
 
@@ -61,8 +61,8 @@ if not User.objects.filter(username='admin').exists():
   # Set some sensible Gunicorn options, needed for things to work with Nginx
 
   # umask working files (worker tmp files & unix socket) as 0o117 (i.e. chmod as
-  # 0o660) so that they are only read/writable by gunicorn and nginx users.
-  set -- su-exec gunicorn "$@" \
+  # 0o660) so that they are only read/writable by django and nginx users.
+  set -- su-exec django "$@" \
     --pid /var/run/gunicorn/gunicorn.pid \
     --bind unix:/var/run/gunicorn/gunicorn.sock \
     --umask 0117 \
