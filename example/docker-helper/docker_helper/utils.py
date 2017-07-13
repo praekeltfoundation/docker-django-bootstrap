9521cc32af0ee186b0641dc3a3b4211502f4d873
Jamie Hewland <jhewland@gmail.com>
2017-07-13 13:00:35 +0200

    Improve the wait_for_log_line function to raise better errors

--- example/docker-helper/docker_helper/utils.py
+++ example/docker-helper/docker_helper/utils.py
@@ -1,46 +1,39 @@
-import _thread as thread
 import re
-import sys
-import threading
+
+from stopit import SignalTimeout, TimeoutException
 
 
 def resource_name(name, namespace='test'):
     return '{}_{}'.format(namespace, name)
 
 
-def _quit_function(fn_name):
-    # https://stackoverflow.com/a/31667005
-    print('{} took too long'.format(fn_name), file=sys.stderr)
-    sys.stderr.flush()  # Python 3 stderr is likely buffered.
-    # FIXME: Interrupting the main thread is hacky
-    thread.interrupt_main()  # raises KeyboardInterrupt
+def _last_few_log_lines(container, max_lines=10):
+    logs = container.logs(tail=max_lines).decode('utf-8')
+    return '\nLast few log lines:\n{}'.format(logs)
 
 
-def exit_after(timeout):
-    """
-    Use as decorator to exit process if function takes longer than s seconds
-    https://stackoverflow.com/a/31667005
-    """
-    def outer(fn):
-        def inner(*args, **kwargs):
-            timer = threading.Timer(
-                timeout, _quit_function, args=[fn.__name__])
-            timer.start()
-            try:
-                result = fn(*args, **kwargs)
-            finally:
-                timer.cancel()
-            return result
-        return inner
-    return outer
+def wait_for_log_line(container, pattern, timeout=10):
+    matched_line = None
+    try:
+        # stopit.ThreadingTimeout doesn't seem to work but a Unix-only
+        # solution should be fine for now :-/
+        with SignalTimeout(timeout):
+            for line in container.logs(stream=True):
+                # Drop the trailing newline
+                line = line.decode('utf-8').rstrip()
+                if re.search(pattern, line):
+                    matched_line = line
+                    break
+    except TimeoutException as e:
+        # In Python 3 we have TimeoutError
+        raise TimeoutError('Timeout waiting for log pattern {!r}.{}'.format(
+            pattern, _last_few_log_lines(container)))
 
+    if matched_line is None:
+        raise RuntimeError('Log pattern {!r} not found in logs.{}'.format(
+            pattern, _last_few_log_lines(container)))
 
-@exit_after(10)
-def wait_for_log_line(container, pattern):
-    for line in container.logs(stream=True):
-        line = line.decode('utf-8').rstrip()  # Drop the trailing newline
-        if re.search(pattern, line):
-            return line
+    return matched_line
 
 
 def output_lines(raw_output, encoding='utf-8'):
