c45c9824ab0e16d15bef557907e80c831853ee26
Jeremy Thurgood <jeremy@praekelt.org>
2017-10-02 18:17:56 +0200

    Move fixtrue creation logic to classmethods.

--- example/fixtures.py
+++ example/fixtures.py
@@ -9,6 +9,9 @@ from seaworthy.pytest.fixtures import clean_container_fixtures
 from seaworthy.utils import output_lines
 
 
+DDB_IMAGE = pytest.config.getoption('--django-bootstrap-image')
+
+
 raw_db_container, db_container = clean_container_fixtures(
     PostgreSQLContainer(), 'db_container', scope='module')
 
@@ -24,77 +27,58 @@ class DjangoBootstrapContainer(ContainerBase):
     def exec_find(self, params):
         return output_lines(self.inner().exec_run(['find'] + params))
 
-
-def create_django_bootstrap_container(
-        request, name, wait_lines, command=None, single_container=False,
-        publish_port=True, other_fixtures=()):
-    for fix in other_fixtures:
-        request.getfixturevalue(fix)
-    image = request.getfixturevalue('django_bootstrap_image')
-    # FIXME: Get these URLs in a better way.
-    database_url = PostgreSQLContainer().database_url()
-    celery_broker_url = RabbitMQContainer(vhost='/mysite').broker_url()
-    kwargs = {
-        'command': command,
-        'environment': {
+    @classmethod
+    def for_fixture(
+            cls, request, name, wait_lines, command=None, needs_db=True,
+            env_extra={}, publish_port=True):
+        amqp_container = request.getfixturevalue('amqp_container')
+        env = {
             'SECRET_KEY': 'secret',
             'ALLOWED_HOSTS': 'localhost,127.0.0.1,0.0.0.0',
-            'DATABASE_URL': database_url,
-            'CELERY_BROKER_URL': celery_broker_url,
-        },
-    }
-    if single_container:
-        kwargs['environment'].update({
-            'CELERY_WORKER': '1',
-            'CELERY_BEAT': '1',
-        })
-    if publish_port:
-        kwargs['ports'] = {'8000/tcp': ('127.0.0.1',)}
-
-    return DjangoBootstrapContainer(name, image, wait_lines, kwargs)
-
-
-def container_factory_fixture(factory, name, kwargs, scope='function'):
-    @pytest.fixture(name=name, scope=scope)
-    def raw_fixture(request, docker_helper):
-        container = factory(request, **kwargs)
-        container.create_and_start(docker_helper)
-        yield container
-        container.stop_and_remove(docker_helper)
-
-    return raw_fixture
-
-
-def make_app_container(
-        name, container_name, other_fixtures, wait_lines, command=None,
-        single_container=False, publish_port=True):
-    return container_factory_fixture(
-        create_django_bootstrap_container, name, kwargs={
-            'name': container_name,
-            'wait_lines': wait_lines,
+            'CELERY_BROKER_URL': amqp_container.broker_url(),
+        }
+        if needs_db:
+            db_container = request.getfixturevalue('db_container')
+            env['DATABASE_URL'] = db_container.database_url()
+        env.update(env_extra)
+        kwargs = {
             'command': command,
-            'single_container': single_container,
-            'publish_port': publish_port,
-            'other_fixtures': other_fixtures,
-        })
+            'environment': env,
+        }
+        if publish_port:
+            kwargs['ports'] = {'8000/tcp': ('127.0.0.1',)}
+
+        return cls(name, DDB_IMAGE, wait_lines, kwargs)
+
+    @classmethod
+    def make_fixture(cls, fixture_name, name, *args, **kw):
+        @pytest.fixture(name=fixture_name)
+        def fixture(request, docker_helper):
+            container = cls.for_fixture(request, name, *args, **kw)
+            container.create_and_start(docker_helper)
+            yield container
+            container.stop_and_remove(docker_helper)
+        return fixture
 
 
-single_container = make_app_container(
-    'single_container', 'web', ['db_container', 'amqp_container'],
+single_container = DjangoBootstrapContainer.make_fixture(
+    'single_container', 'web',
     [r'Booting worker', r'celery@\w+ ready', r'beat: Starting\.\.\.'],
-    single_container=True)
+    env_extra={'CELERY_WORKER': '1', 'CELERY_BEAT': '1'})
 
-web_only_container = make_app_container(
-    'web_only_container', 'web', ['db_container', 'amqp_container'],
-    [r'Booting worker'])
 
-worker_only_container = make_app_container(
-    'worker_only_container', 'worker', ['amqp_container'],
-    [r'celery@\w+ ready'], command=['celery', 'worker'], publish_port=False)
+web_only_container = DjangoBootstrapContainer.make_fixture(
+    'web_only_container',  'web', [r'Booting worker'])
 
-beat_only_container = make_app_container(
-    'beat_only_container', 'beat', ['amqp_container'],
-    [r'beat: Starting\.\.\.'], command=['celery', 'beat'], publish_port=False)
+
+worker_only_container = DjangoBootstrapContainer.make_fixture(
+    'worker_only_container', 'worker', [r'celery@\w+ ready'],
+    command=['celery', 'worker'], publish_port=False)
+
+
+beat_only_container = DjangoBootstrapContainer.make_fixture(
+    'beat_only_container', 'beat', [r'beat: Starting\.\.\.'],
+    command=['celery', 'beat'], publish_port=False)
 
 
 def make_multi_container(name, containers):
