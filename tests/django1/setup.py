da0155f5679b9da570a17de2ab73768b8277a337
Jamie Hewland <jhewland@gmail.com>
2018-07-05 13:22:04 +0200

    tests/django1: Remove amqp pin.. no reason for someone to install this version

--- tests/django1/setup.py
+++ tests/django1/setup.py
@@ -8,9 +8,6 @@ setup(
     packages=['mysite'],
     install_requires=[
         'celery >=4.2, <4.3',
-        # This version seems to be broken with Python 3:
-        # https://github.com/celery/py-amqp/issues/155
-        'amqp != 2.2.0',
         'Django >=1.11, <1.12',
         'django_compressor >=2.1',
         'django-environ',
