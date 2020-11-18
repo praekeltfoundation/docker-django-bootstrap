2b7757e5a1149906b9b58deca4e4a282c75686a6
Jeremy Thurgood <jeremy@praekelt.org>
2020-11-18 10:39:06 +0200

    More django1 test dep tweaks

--- tests/django1/setup.py
+++ tests/django1/setup.py
@@ -6,16 +6,13 @@ from setuptools import setup
 install_requires = [
     'celery >=4.2, <4.3',
     'Django >=1.11, <1.12',
-    'django-appconf < 1.0.3',  # Last version with py27 support.
+    'django-appconf <1.0.3',  # Last version with py27 support.
     'django_compressor >=2.1, <2.4',  # Last version with py27 support.
     'django-environ',
-    'django-prometheus',
+    'django-prometheus <2',
     'psycopg2 >=2.7',
+    'django-health-check <3',
 ]
-if sys.version_info[0] < 3:
-    install_requires.append('django-health-check <3')
-else:
-    install_requires.append('django-health-check')
 
 setup(
     name='mysite',
